5c10fe92b00e4464964d8cfb4249168d88af6ce0
Tim Dorr <git@timdorr.com>
2021-10-18 16:32:20 -0400

    Set up the API client

---
+++ pkg/client/client.go
@@ -0,0 +1,72 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/onspaceship/ship/pkg/token"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	defaultCoreBaseURL = "https://core.onspaceship.com/"
+)
+
+type Client struct {
+	http    *http.Client
+	baseURL *url.URL
+	token   string
+}
+
+func NewClient() *Client {
+	baseURL, err := url.Parse(viper.GetString("core_base_url"))
+	if err != nil {
+		log.Fatalf("Invalid Core base URL: %s", viper.GetString("core_base_url"))
+	}
+
+	http := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	return &Client{
+		http:    http,
+		baseURL: baseURL,
+		token:   token.GetToken(),
+	}
+}
+
+func (client *Client) Get(url string) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", client.token))
+
+	return client.http.Do(req)
+}
+
+func (client *Client) GetJSON(url string, data interface{}) error {
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return json.NewDecoder(resp.Body).Decode(data)
+}
+
+func (client *Client) corePath(path string, tokens ...interface{}) string {
+	url, _ := client.baseURL.Parse(fmt.Sprintf(path, tokens...))
+	return url.String()
+}
+
+func init() {
+	viper.SetDefault("core_base_url", defaultCoreBaseURL)
+}
